bd7062051a99f1a33683b420fc5e7fb72df8ba24
Pablo Gago <pablogago@Mac-mini-de-Pablo.local>
2022-03-25 14:20:04 +0100

    inicio repositorio

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>&nbsp;
+    <router-link to="/about">Acerca de</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
